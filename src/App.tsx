8f823f326218c7db940ba1cf64695020b0869097
Tim <timschoenwald@googlemail.com>
2022-02-15 14:25:06 +0100

    added state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useRef, useState } from "react";
 import "./App.scss";
 
+const canvasX = 1000;
+const canvasY = 1000;
+const initialSnake = [
+  [4, 10],
+  [4, 10],
+];
+const initialApple = [14, 10];
+const scale = 50;
+const timedelay = 100;
+
 function App() {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [delay, setDelay] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+
   return <div className='App'>Test</div>;
 }
 
